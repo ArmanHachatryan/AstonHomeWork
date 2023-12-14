b6497e9fe32a429128c96290e7728e6d8b5464b3
Arman Hachatryan <winnermmr7@mail.ru>
2023-12-14 16:00:54 +0400

    Done lesson_2

--- Start/src/main/java/ru/aston/lessons/Main.java
+++ Start/src/main/java/ru/aston/lessons/Main.java
@@ -2,10 +2,51 @@ package ru.aston.lessons;
 
 public class Main {
     public static void main(String[] args) {
-        hello();
+        printThreeWords();
+
+        checkSumSign();
+
+        printColor();
+
+        compareNumber();
     }
 
-    public static void hello(){
-        System.out.println("Lesson_1");
+    public static void printThreeWords(){
+        System.out.println("Orange\nBanana\nApple");
     }
+
+    public static void checkSumSign() {
+        int a, b;
+        a = 5;
+        b = -10;
+        if ((a+b) >= 0) {
+            System.out.println("Сумма положительная");
+        }
+        else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+
+    public static void printColor(){
+        var value = 100;
+        if (value <= 0) {
+            System.out.println("Красный");
+        }
+        else if (value <= 100){
+            System.out.println("Желтый");
+        }
+        else {
+            System.out.println("Зеленый");
+        }
+    }
+
+    public static void compareNumber() {
+        int a,b;
+        a = 5;
+        b = 3;
+
+        if (a >= b) System.out.println("a >= b");
+        else System.out.println("a < b");
+    }
+
 }
