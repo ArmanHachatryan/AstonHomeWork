f82d8998b20c080abae88afe89340069ebef7d81
Arman Hachatryan <winnermmr7@mail.ru>
2023-12-14 15:56:15 +0400

    Lesson_1

---
+++ Start/src/main/java/ru/aston/lessons/Main.java
@@ -0,0 +1,11 @@
+package ru.aston.lessons;
+
+public class Main {
+    public static void main(String[] args) {
+        hello();
+    }
+
+    public static void hello(){
+        System.out.println("Lesson_1");
+    }
+}
