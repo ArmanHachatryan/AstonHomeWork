035c8530405762efde739f593341b54a276bc0ae
Арман <58301912+ArmanHachatryan@users.noreply.github.com>
2023-12-21 08:58:09 +0300

    1-4

--- Start/src/main/java/ru/aston/lessons/Main.java
+++ Start/src/main/java/ru/aston/lessons/Main.java
@@ -2,7 +2,14 @@ package ru.aston.lessons;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(MyMethods1.SumBetween10And20(5, 14));
+
+        System.out.println(MyMethods1.sumBetween10And20(5, 14));
+
+        MyMethods1.isPositive(-4);
+
+        System.out.println(MyMethods1.isNegative(-4));
+
+        MyMethods1.printStr("hey", 3);
     }
 
 }
--- Start/src/main/java/ru/aston/lessons/MyMethods1.java
+++ Start/src/main/java/ru/aston/lessons/MyMethods1.java
@@ -3,9 +3,23 @@ package ru.aston.lessons;
 public final class MyMethods1 {
     private MyMethods1() {}
 
-    public static boolean SumBetween10And20(int a, int b){
+    public static boolean sumBetween10And20(int a, int b){
         int sum = a + b;
         return (10 <= sum) && (sum <= 20);
     }
 
+    public static void isPositive(int a){
+        if (a < 0) System.out.println("Отрицательное");
+        else System.out.println("Положительное");
+    }
+
+    public static boolean isNegative(int a){
+        return a < 0;
+    }
+
+    public static void printStr(String text, int b){
+        for (int i = 0; i < b; i++){
+            System.out.println(text);
+        }
+    }
 }
