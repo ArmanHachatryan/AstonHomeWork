f82c54b00c567c678638bf22eb2617e030534eb2
Arman Hachatryan <winnermmr7@mail.ru>
2024-01-11 20:56:26 +0400

    add table

--- Start/src/main/java/ru/aston/lessons/Main.java
+++ Start/src/main/java/ru/aston/lessons/Main.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
--- Start/src/main/java/ru/aston/lessons/Reader.java
+++ Start/src/main/java/ru/aston/lessons/Reader.java
@@ -3,6 +3,7 @@ package ru.aston.lessons;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.stream.Stream;
 
 public class Reader {
 
@@ -13,9 +14,11 @@ public class Reader {
         try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
             String str;
             while ((str = reader.readLine()) != null) {
-                System.out.println(str);
+                Stream<String> ms = Stream.of(str);
+                ms.flatMap(p -> Stream.of(
+                        String.format("%-8s | %-8s | %-8s |", p.split(";")[0], p.split(";")[1], p.split(";")[2])
+                )).forEach(s -> System.out.println(s + "\n" + "_______________________________"));
             }
-
             System.out.println("Файл прочитан");
         } catch (IOException e) {
             e.printStackTrace();
