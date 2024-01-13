084730413907c93f9bf761ac04352f4a38c35aa1
Arman Hachatryan <winnermmr7@mail.ru>
2024-01-13 15:56:05 +0400

    Update Phonebook.java

--- Start/src/main/java/ru/aston/lessons/Phonebook.java
+++ Start/src/main/java/ru/aston/lessons/Phonebook.java
@@ -1,26 +1,24 @@
 package ru.aston.lessons;
 
-import java.security.Key;
 import java.util.*;
-import java.util.stream.LongStream;
 import java.util.stream.Stream;
 
 public class Phonebook {
-    private final HashMap<String, ArrayList<Long>> hs = new HashMap<>();
+    private final HashMap<String, ArrayList<Long>> hashMap = new HashMap<>();
 
     public void add(String lastName, long phoneNumber) {
-        if (hs.containsKey(lastName)) {
-            hs.get(lastName).add(phoneNumber);
+        if (hashMap.containsKey(lastName)) {
+            hashMap.get(lastName).add(phoneNumber);
         } else {
-            ArrayList<Long> al = new ArrayList<>();
-            al.add(phoneNumber);
-            hs.put(lastName, al);
+            ArrayList<Long> phoneNumberArray = new ArrayList<>();
+            phoneNumberArray.add(phoneNumber);
+            hashMap.put(lastName, phoneNumberArray);
         }
     }
 
     public void get(String lastName) {
         System.out.println(lastName);
-        Stream<ArrayList<Long>> longStream = Stream.of(hs.get(lastName));
+        Stream<ArrayList<Long>> longStream = Stream.of(hashMap.get(lastName));
         longStream.flatMap(Collection::stream).forEach(System.out::println);
     }
 }
