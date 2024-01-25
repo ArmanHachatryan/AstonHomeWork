5f81f1bf8d3a900dbf5a81084a43942d377730c9
Arman Hachatryan <winnermmr7@mail.ru>
2024-01-25 19:06:13 +0400

    end

--- MavenQuickStart/src/test/java/org/aston/exercise/AppTest.java
+++ MavenQuickStart/src/test/java/org/aston/exercise/AppTest.java
@@ -1,6 +1,22 @@
 package org.aston.exercise;
 
-public class AppTest
-{
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
+public class AppTest {
+    @DataProvider(name = "data")
+    public Integer[][] data() {
+        return new Integer[][]{
+                {0, 1},
+                {1, 1},
+                {2, 2},
+                {3, 6},
+        };
+    }
+
+    @Test(dataProvider = "data")
+    public void checkFactorial(int arg, long expected) {
+        Assert.assertEquals(expected, App.calcFactorial(arg));
+    }
 }
