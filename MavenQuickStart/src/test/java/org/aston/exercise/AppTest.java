10ea420843ba9e6fb6471dae36f7ed384c1b1c21
Arman Hachatryan <winnermmr7@mail.ru>
2024-01-25 16:19:01 +0400

    end

--- MavenQuickStart/src/test/java/org/aston/exercise/AppTest.java
+++ MavenQuickStart/src/test/java/org/aston/exercise/AppTest.java
@@ -1,6 +1,25 @@
 package org.aston.exercise;
 
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.stream.Stream;
+
 public class AppTest
 {
-
+    @DisplayName("Проверка вычисления факториала чисел")
+    @ParameterizedTest
+    @CsvSource({
+            "0, 1",
+            "1, 1",
+            "2, 2",
+            "3, 6",
+            "5, 120",
+    })
+    public void checkFactorial(int argument, long expected) {
+        Assertions.assertEquals(expected, App.calcFactorial(argument));
+    }
 }
