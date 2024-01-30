9885ca2755d5a0bf49552087df3e3a20bc7ba04a
Arman Hachatryan <winnermmr7@mail.ru>
2024-01-30 20:35:44 +0400

    start

--- MavenQuickStart/src/main/java/org/aston/exercise/App.java
+++ MavenQuickStart/src/main/java/org/aston/exercise/App.java
@@ -4,11 +4,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println(calcFactorial(5));
-    }
 
-    public static long calcFactorial(int number) {
-        if (number < 1) return 1;
-        return number * calcFactorial(number - 1);
     }
 }
