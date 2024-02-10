e2c0be59ee5cc26408580f65aeb95d168b44bd95
Arman Hachatryan <winnermmr7@mail.ru>
2024-02-10 20:13:13 +0400

    started

--- MavenQuickStart/src/test/java/org/aston/exercise/AppTest.java
+++ MavenQuickStart/src/test/java/org/aston/exercise/AppTest.java
@@ -1,6 +1,19 @@
 package org.aston.exercise;
 
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class AppTest
 {
-
+    @Test
+    public void whenCreatePerson_thenStatus201() {
+        RestAssured.given()
+                .baseUri("https://reqres.in/")
+                .when()
+                .get("api/users?page=2")
+                .then()
+                .assertThat()
+                .statusCode(200);
+    }
 }
