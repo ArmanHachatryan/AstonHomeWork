8c69a9d120e9fd3b0feebe85936ad56add3e8440
Arman Hachatryan <winnermmr7@mail.ru>
2024-02-16 14:57:53 +0400

    start

--- MavenQuickStart/src/test/java/org/aston/exercise/AppTest.java
+++ MavenQuickStart/src/test/java/org/aston/exercise/AppTest.java
@@ -1,6 +1,47 @@
 package org.aston.exercise;
 
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.remote.AndroidMobileCapabilityType;
+import io.appium.java_client.remote.MobileCapabilityType;
+import io.appium.java_client.android.AndroidDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class AppTest
 {
+    AndroidDriver<AndroidElement> driver = null;
 
+    public void initialize() {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME,"Pixel 7 Pro");
+        capabilities.setCapability(MobileCapabilityType.APP,"C:\\Users\\winne\\Downloads\\Calculator_8.4.1 (520193683)_Apkpure.apk");
+
+        capabilities.setCapability(MobileCapabilityType.NO_RESET,true);
+        try {
+            driver = new AndroidDriver<>(new
+                    URL("http://127.0.0.1:4723/wd/hub"),capabilities);
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        } catch (MalformedURLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Test
+    public void checkCalculate() {
+        initialize();
+        driver.findElementById("com.google.android.calculator:id/digit_3").click();
+
+        driver.findElementById("com.google.android.calculator:id/op_add").click();
+        driver.findElementById("com.google.android.calculator:id/digit_9").click();
+
+        driver.findElementById("com.google.android.calculator:id/eq").click();
+        assertEquals("12", driver.findElementById("com.google.android.calculator:id/result_final").getText());
+    }
 }
