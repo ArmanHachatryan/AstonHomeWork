42206da4412e20828441240f59eae78d902b42b2
Arman Hachatryan <winnermmr7@mail.ru>
2024-02-18 12:24:29 +0400

    testng

--- MavenQuickStart/src/test/java/org/aston/exercise/CalculatorFunctionTest.java
+++ MavenQuickStart/src/test/java/org/aston/exercise/CalculatorFunctionTest.java
@@ -1,18 +1,24 @@
 package org.aston.exercise;
 
 import org.aston.exercise.baseTest.BaseTest;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
 public class CalculatorFunctionTest extends BaseTest {
-    @ParameterizedTest
-    @DisplayName("Проверить базовые операции калькулятора")
-    @CsvSource({"3 + 9 + 0, 12", "5 - 5 + 9, 9", "2 * 2, 4", "6 / 3, 2"})
+    @DataProvider
+    public String[][] Expressions() {
+        return new String[][] {
+                {"3 + 9 + 0", "12"},
+                {"5 - 5 + 9", "9"},
+                {"2 * 2", "4"},
+                {"6 / 3", "2"},
+        };
+    }
+
+    @Test(dataProvider = "Expressions")
     public void checkBaseOperations(String expression, String expectedResult) {
         String actualResult = mainPage.enterExpression(expression).getResult();
-        assertEquals(expectedResult, actualResult);
+        Assert.assertEquals(expectedResult, actualResult);
     }
 }
--- MavenQuickStart/src/test/java/org/aston/exercise/baseTest/BaseTest.java
+++ MavenQuickStart/src/test/java/org/aston/exercise/baseTest/BaseTest.java
@@ -4,20 +4,20 @@ import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 import org.aston.exercise.pages.MainPage;
 import org.aston.exercise.utils.DriverInstance;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 
 public class BaseTest {
     protected static AndroidDriver<AndroidElement> driver;
     protected MainPage mainPage = PageFactory.initElements(driver, MainPage.class);
 
-    @BeforeAll
+    @BeforeClass
     public static void setup() {
         driver = DriverInstance.getDriver();
     }
 
-    @AfterAll
+    @AfterClass
     public static void teardown() {
         DriverInstance.quit();
     }
