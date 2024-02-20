f0494d03529d94d486b77d6e5b0a8bddf8a483f7
Arman Hachatryan <winnermmr7@mail.ru>
2024-02-20 19:25:15 +0400

    fix

--- MavenQuickStart/src/test/java/org/aston/exercise/CalculatorFunctionTest.java
+++ MavenQuickStart/src/test/java/org/aston/exercise/CalculatorFunctionTest.java
@@ -1,13 +1,16 @@
 package org.aston.exercise;
 
 import org.aston.exercise.baseTest.BaseTest;
+import org.aston.exercise.pages.MainPage;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.openqa.selenium.support.PageFactory;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalculatorFunctionTest extends BaseTest {
+    protected MainPage mainPage = PageFactory.initElements(driver, MainPage.class);
     @ParameterizedTest
     @DisplayName("Проверить базовые операции калькулятора")
     @CsvSource({"3 + 9 + 0, 12", "5 - 5 + 9, 9", "2 * 2, 4", "6 / 3, 2"})
--- MavenQuickStart/src/test/java/org/aston/exercise/baseTest/BaseTest.java
+++ MavenQuickStart/src/test/java/org/aston/exercise/baseTest/BaseTest.java
@@ -2,15 +2,13 @@ package org.aston.exercise.baseTest;
 
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
-import org.aston.exercise.pages.MainPage;
 import org.aston.exercise.utils.DriverInstance;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
-import org.openqa.selenium.support.PageFactory;
+
 
 public class BaseTest {
     protected static AndroidDriver<AndroidElement> driver;
-    protected MainPage mainPage = PageFactory.initElements(driver, MainPage.class);
 
     @BeforeAll
     public static void setup() {
